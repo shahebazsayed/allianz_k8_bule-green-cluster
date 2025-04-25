88a3cfcb048c9e48b70da3f08d1207617a98eca0
shahebaz <35893376+shahebazsayed@users.noreply.github.com>
2025-04-25 17:13:28 +0530

    Update app.js

--- allianz-bluegreen-deployment/nodejs-app/app.js
+++ allianz-bluegreen-deployment/nodejs-app/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-app.get('/', (req, res) => res.send(`Hello from ${process.env.VERSION || 'blue'} version!`));
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send(`Hello from ${process.env.VERSION || "blue"} version!`);
+});
+app.listen(port, () => console.log(`App running on port ${port}`));
